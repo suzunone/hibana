dc0c5d91e80ecd160422f0000d90f870ce6ef5d3
suzunone <suzunone.eleven@gmail.com>
2026-05-17 14:57:37 +0900

    fixed memory leak

--- src/Console/Commands/GenerateConsole.php
+++ src/Console/Commands/GenerateConsole.php
@@ -10,6 +10,8 @@ use Suzunone\Hibana\Contracts\StaticSiteFactory;
 use Suzunone\Hibana\Contracts\StaticSiteGenerator;
 use Suzunone\Hibana\Simulations\HttpRequestSimulation;
 use Symfony\Component\Console\Attribute\AsCommand;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Blade;
 
 #[AsCommand(name: 'hibana:generator')]
 class GenerateConsole extends Command
@@ -43,6 +45,11 @@ class GenerateConsole extends Command
     {
         $httpRequest = $this->httpRequestSimulation();
 
+        $iterate = 0;
+
+        app()->make('view.engine.resolver')->register('blade', function () {
+            return new \Illuminate\View\Engines\CompilerEngine(app()->get('blade.compiler'));
+        });
         foreach ($this->config->get('hibana.generators') as $generator_name) {
             $generator = new $generator_name;
             throw_unless($generator instanceof StaticSiteGenerator, RuntimeException::class, sprintf('%s does not implement %s', $generator_name, StaticSiteGenerator::class));
@@ -63,7 +70,35 @@ class GenerateConsole extends Command
                     ->path($this->config->get('hibana.artifact_path') . $factory->savePath());
 
                 $this->components->info(sprintf('Static contents %s [%s] created successfully.', $path, $factory->url()));
+
+                if ($iterate++ >= 100) {
+                    // 1. ビューの状態をクリア
+                    View::flushState();
+
+                    // ② ViewFinderが内部にキャッシュしている「解決済みファイルパス」のマップをクリア
+                    // ※これをしないと、ファイルパスの文字列がループの数だけメモリに蓄積されます
+                    if (app()->bound('view.finder')) {
+                        app('view.finder')->flush();
+                    }
+
+                    // 3. コンテナにキャッシュされているViewファクトリ自体を再生成（※特に有効）
+                    app()->forgetInstance('view');
+                    app()->make('view');
+                    app()->forgetInstance('blade.compiler');
+                    app()->forgetInstance('view.engine.resolver');
+
+                    app()->make('view.engine.resolver')->register('blade', function () {
+                        return new \Illuminate\View\Engines\CompilerEngine(app()->get('blade.compiler'));
+                    });
+
+                    // 4. PHPにメモリ解放を促す
+                    gc_collect_cycles();
+
+                    $this->components->info('flushState!!');
+                    $iterate = 0;
+                }
             }
+
         }
 
     }
